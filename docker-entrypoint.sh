84e7f6d18c84e6f735e4c82e4ec5e107e0e7e7bc
klsmith-usgs <kelcy.smith.ctr@usgs.gov>
2019-02-28 09:54:12 -0600

    minor changes to entrypoint shell script from suggestions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,40 +2,40 @@
 
 # Default to the built-in lcmap user for step-down.
 uid=${1:-1000}
-uname=${2:-"lcmap"}
+usern=${2:-"lcmap"}
 gid=${3:-100}
-gname=${4:-"users"}
+groupn=${4:-"users"}
 
 # Could if out everything, but let's let the user assume some responsibility (what could go wrong?).
 
 # What could go wrong?
-if [ $gname = "users" -a $gid != 100 ]; then
+if [ ${groupn} = "users" -a ${gid} != 100 ]; then
   echo "Changing group name to avoid conflict"
   gname="somegroup"
 fi
 
 # Different group permissions.
-if [ $gid != 100 ]; then
-  echo "Adding group ${gname} ${gid}"
-  groupadd -f --gid=${gid} "${gname}"
+if [ ${gid} != 100 ]; then
+  echo "Adding group ${groupn} ${gid}"
+  groupadd -f --gid=${gid} "${groupn}"
 fi
 
 # Modify lcmap user, name is not important.
-if [ $uid != 1000 -a $uname = "lcmap" ]; then
+if [ ${uid} != 1000 -a ${usern} = "lcmap" ]; then
   echo "Modifying lcmap user"
-  usermod --gid=${gid} --uid=$uid --login="${uname}" lcmap
+  usermod --gid=${gid} --uid=${uid} --login="${usern}" lcmap
 fi
 
 # Add unique user to step-down to.
-if [ $uid != 1000 -a $uname != "lcmap" ]; then
+if [ ${uid} != 1000 -a ${usern} != "lcmap" ]; then
   echo "Adding new user to container"
-  useradd -r -m -g $gid --uid=$uid "${uname}"
+  useradd -r -m -g ${gid} --uid=${uid} "${usern}"
 fi
 
 # Pertinent environment variables related to the user.
-export HOME=/home/"${uname}"
-export USER="${uname}"
+export HOME=/home/"${usern}"
+export USER="${usern}"
 cd ~
 
-echo "Stepping down to ${uname}"
-exec gosu ${uname} "/bin/bash"
+echo "Stepping down to ${usern}"
+exec gosu ${usern} "/bin/bash"
